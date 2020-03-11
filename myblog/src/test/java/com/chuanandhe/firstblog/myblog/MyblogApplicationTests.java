1ee39ee89c49a66974c3b112b90adf261cafd136
chenyingchuan <781238977@qq.com>
2020-03-11 19:39:30 +0800

    搭建项目的初始环境[提交人:陈颖川]

---
+++ myblog/src/test/java/com/chuanandhe/firstblog/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuanandhe.firstblog.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
