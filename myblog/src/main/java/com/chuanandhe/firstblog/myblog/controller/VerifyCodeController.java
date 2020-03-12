3c11cc51176a4c16c6c2c64ec1e9ffa325c52aff
yingchuan.chen <781238977@qq.com>
2020-03-12 11:52:43 +0800

    添加kaptcha验证码的配置文件[提交人:陈颖川]

--- myblog/src/main/java/com/chuanandhe/firstblog/myblog/controller/VerifyCodeController.java
+++ myblog/src/main/java/com/chuanandhe/firstblog/myblog/controller/VerifyCodeController.java
@@ -1,6 +1,7 @@
 package com.chuanandhe.firstblog.myblog.controller;
 
 import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,6 +19,7 @@ import java.io.IOException;
 
 @Controller
 public class VerifyCodeController {
+    @Autowired
     private DefaultKaptcha defaultKaptcha;
 
     @RequestMapping("/verifyCode")
